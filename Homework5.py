8594015ea6c83b28cd0be9019f3241ac0724d224
Dmit130489 <dmit.89@list.ru>
2024-05-19 18:22:54 +0300

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,13 @@
+my_list = ['яблоко', 'груша', 'банан', 'киви', 'персик']
+print(my_list)
+print(my_list[1:5:3])
+print(my_list[3:5])
+my_list[3] = 'kiwi'
+print(my_list)
+
+
+my_dict = {'apple': 'яблоко', 'pear': 'груша', 'kiwi': 'киви'}
+print(my_dict)
+print(my_dict.get('apple'))
+my_dict['orenge'] = 'апельсин'
+print(my_dict)
