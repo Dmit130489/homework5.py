2e962a4eb67286f42ef23c049488acb2639b70bf
Dmit130489 <dmit.89@list.ru>
2024-05-23 20:58:17 +0300

    Homework5.py

--- Homework5.py
+++ Homework5.py
@@ -1,7 +1,7 @@
 my_list = ['яблоко', 'груша', 'банан', 'киви', 'персик']
 print(my_list)
-print(my_list[1:5:3])
-print(my_list[3:5])
+print(my_list[::4])
+print(my_list[2:])
 my_list[3] = 'kiwi'
 print(my_list)
 
